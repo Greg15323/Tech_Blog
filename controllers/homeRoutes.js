280aed275d462b6edb5bb3a77f93fe19a289784d
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-03-04 17:37:49 -0700

    updates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-  const { User } = require('../models');
+  const { User, Post } = require('../models');
  const withAuth = require('../utils/auth');
 
  router.get('/', async (req, res) => {
@@ -20,7 +20,7 @@ const router = require('express').Router();
      // Pass serialized data and session flag into template
      res.render('homepage', { 
     //    projects, 
-    //    logged_in: req.session.logged_in 
+        logged_in: req.session.logged_in 
      });
    } catch (err) {
      res.status(500).json(err);
@@ -50,24 +50,24 @@ const router = require('express').Router();
 //  });
 
  // Use withAuth middleware to prevent access to route
-//  router.get('/profile', withAuth, async (req, res) => {
-//    try {
-//      // Find the logged in user based on the session ID
-//      const userData = await User.findByPk(req.session.user_id, {
-//        attributes: { exclude: ['password'] },
-//        include: [{ model: Project }],
-//      });
+ router.get('/profile', withAuth, async (req, res) => {
+   try {
+     // Find the logged in user based on the session ID
+     const userData = await User.findByPk(req.session.user_id, {
+       attributes: { exclude: ['password'] },
+       include: [{ model: Post }],
+     });
 
-    //  const user = userData.get({ plain: true });
+     const user = userData.get({ plain: true });
 
-//      res.render('profile', {
-//        ...user,
-//        logged_in: true
-//      });
-//    } catch (err) {
-//      res.status(500).json(err);
-//    }
-//  });
+     res.render('profile', {
+       ...user,
+       logged_in: true
+     });
+   } catch (err) {
+     res.status(500).json(err);
+   }
+ });
 
  router.get('/login', (req, res) => {
    // If the user is already logged in, redirect the request to another route
